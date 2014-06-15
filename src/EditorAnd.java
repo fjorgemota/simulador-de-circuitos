f3e854101b1c715166f922a9157975d24355104c
Fernando Jorge Mota <f.j.mota13@gmail.com>
2014-06-15 19:12:28 -0300

    Adaptado editor para adicinar o FigAnd ao editor

--- src/EditorAnd.java
+++ src/EditorAnd.java
@@ -3,17 +3,8 @@ class EditorAnd implements Editor {
 	EditorAnd(Quadro quadro){
 		this.quadro = quadro;
 	}
-	private int npontos;
-	private int x0, y0;
 	public void clique(int x, int y){
-		if(npontos == 0){
-			npontos = 1;
-			x0 = x;
-			y0 = y;
-		}
-		if(npontos == 1){
-			Main.quadro.addFig(new FigRetangulo(new Ponto(x0, y0), new Ponto(x,y)));// novamente, como não sei como será o método de draw, deixarei de um modo "genérico" para posterior análise;
-			Main.quadro.repaint();
-		}		
+        Main.quadro.addFig(new FigAnd(new Ponto(x, y), new Ponto(x, y)));
+        Main.quadro.repaint();
 	}
 }
