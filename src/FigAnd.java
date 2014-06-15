1662c22d4136461745d36510ac1936c069bf5dc8
Fernando Jorge Mota <f.j.mota13@gmail.com>
2014-06-15 19:12:57 -0300

    Adicionado lógica para carregar a imagem e pintar no painel. Entretanto, temos aqui o que melhorar como conversado no Facebook.

--- src/FigAnd.java
+++ src/FigAnd.java
@@ -1,10 +1,24 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 class FigAnd extends And implements Reproduzivel {
+    BufferedImage image;
 	FigAnd(Ponto a, Ponto b){
 		super(a, b);
+        try {
+            image = ImageIO.read(new File("imagens/and.png"));
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao carregar imagem");
+            return;
+        }
+
 	}
 	public void reproduzir(Graphics g) {
-		//desenhar Imagem; Como ainda não sei a realização desse método, deixarei em branco para posteerior análise;
-	}
+        g.drawImage(image, this.x(), this.y(), null);
+
+    }
 }
