f0d91a5385e459e8ab1030cd952778db9e02299a
Fernando Jorge Mota <f.j.mota13@gmail.com>
2014-07-05 21:48:56 -0300

    Adiciona métodos úteis para selecionar e mover reproduziveis.

--- src/views/Reproduzivel.java
+++ src/views/Reproduzivel.java
@@ -2,6 +2,9 @@ package views;
 
 import java.awt.*;
 
-interface Reproduzivel {
+public interface Reproduzivel {
     public void reproduzir(Graphics g);
+    public boolean contemPonto(int x, int y);
+    public void selecionaPonto(int x, int y);
+    public void movePara(int x, int y);
 }
