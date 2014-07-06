d17d43ecb07887fdbb400d9c7330168b8a0573c0
Fernando Jorge Mota <f.j.mota13@gmail.com>
2014-07-05 21:09:25 -0300

    Adicionado atributo para guardar uma referência à porta lógica associada à entrada ou saida. É usado para calcular dinamicamente a posição x e y da entrada/saida.

--- src/models/Entrada.java
+++ src/models/Entrada.java
@@ -3,7 +3,16 @@ package models;
 import models.Ponto;
 
 public class Entrada extends Circulo {
-    Entrada(int x, int y) {
+    PortaLogica referencia;
+    Entrada(PortaLogica referencia, int x, int y) {
         super(x, y, 5);
+        this.referencia = referencia;
+    }
+
+    public int x0() {
+        return this.referencia.x() + super.x0();
+    }
+    public int y0() {
+        return this.referencia.y() + super.y0() ;
     }
 }
--- src/models/Saida.java
+++ src/models/Saida.java
@@ -3,7 +3,16 @@ package models;
 import models.Ponto;
 
 public class Saida extends Circulo {
-    Saida(int x, int y) {
+    PortaLogica referencia;
+    Saida(PortaLogica referencia, int x, int y) {
         super(x, y, 5);
+        this.referencia = referencia;
+    }
+
+    public int x0() {
+        return this.referencia.x() + super.x0();
+    }
+    public int y0() {
+        return this.referencia.y() + super.y0() ;
     }
 }
